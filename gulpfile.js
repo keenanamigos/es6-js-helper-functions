98c4016b2a26a3b3d383d3e06535ef05ecc49e25
Keenan Johnson <keenan.johnson1@outlook.com>
2017-08-09 23:14:49 -0500

    Add gulp and print packages. Files compiling to 'dist' directory and tests passing

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 const babel = require('gulp-babel');
+const gulp = require('gulp');
+const print = require('gulp-print');
+
 
 gulp.task('js', () => {
     return gulp.src('src/*.js')
         .pipe(print())
-        .pipe(babel({ presets: ['es2015'] }))
+        .pipe( babel({ presets: ['es2015'] }))
         .pipe(gulp.dest('dist'));
 });
