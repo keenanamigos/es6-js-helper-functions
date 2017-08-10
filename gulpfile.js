d1ccfc9a672c42c9fe3967f657ad1213d7288abd
Keenan Johnson <keenan.johnson1@outlook.com>
2017-08-09 23:17:49 -0500

    Change task name from 'js' to 'build'
    Add 'watch' task to gulp to build compiled JS files on ES6 file change

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,13 @@ const gulp = require('gulp');
 const print = require('gulp-print');
 
 
-gulp.task('js', () => {
+gulp.task('build', () => {
     return gulp.src('src/*.js')
         .pipe(print())
         .pipe( babel({ presets: ['es2015'] }))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('src/*.js', ['build']);
 });
